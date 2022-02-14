b9735c6ede6e3c0ea1a8bd84de11dbea54ea029b
joshmay222 <joshmay222@gmail.com>
2022-02-14 08:15:34 -0600

    cleaned URLs & added enter functionality

--- index.js
+++ index.js
@@ -1,11 +1,39 @@
 'use strict';
 
 const button = document.querySelector('.btn');
-const inputs = document.querySelector('.inputs');
+const inputField = document.querySelector('.inputs');
 const p = document.querySelector('.p');
 
 button.addEventListener('click', mergeURLs);
 
+window.addEventListener('keypress', function (event) {
+  if (event.keyCode === 13) {
+    mergeURLs();
+    event.preventDefault();
+  }
+});
+
 function mergeURLs() {
-  p.textContent = inputs.value.split('\n').join('|');
+  let inputValueArr = inputField.value.split('\n');
+  let arr = [];
+
+  for (const clean of inputValueArr) {
+    arr.push(
+      clean
+        .substring(clean.indexOf('z') + 1)
+        .split('#'[0])
+        .slice(0, 1)
+    );
+  }
+
+  p.textContent = arr.join('|');
 }
+
+/* ------NEXT------
+        
+// Need to clean code for other TLD - .com, .co.uk.....
+// Need to add the () around the finished project
+        
+
+
+*/
