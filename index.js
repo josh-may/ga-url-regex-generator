2d0be4fcc8d639059e7eb66d1c7becb214c56137
joshmay222 <joshmay222@gmail.com>
2022-03-09 15:12:10 -0600

    fixed ".com"

--- index.js
+++ index.js
@@ -19,49 +19,51 @@ function mergeURLs() {
   let com = '.com';
   let questionMark = '?';
 
-  if (inputField.value.includes(questionMark)) {
-    if (inputField.value.includes(com)) {
-      for (const clean of inputValueArr) {
-        arr.push(
-          clean
-            .substring(clean.indexOf('com') + 3)
-            .split('?'[0])
-            .slice(0, 1)
-        );
-      }
-    } else {
-      for (const clean of inputValueArr) {
-        arr.push(
-          clean
-            .substring(clean.indexOf('xyz') + 3)
-            .split('?'[0])
-            .slice(0, 1)
-        );
-      }
+  
+if (inputField.value.includes(questionMark)) {
+  if (inputField.value.includes(com)) {
+    for (const clean of inputValueArr) {
+      arr.push(
+        clean
+          .substring(clean.indexOf('.com') + 4)
+          .split('?'[0])
+          .slice(0, 1)
+      );
     }
   } else {
-    if (inputField.value.includes(com)) {
-      for (const clean of inputValueArr) {
-        arr.push(
-          clean
-            .substring(clean.indexOf('com') + 3)
-            .split('#'[0])
-            .slice(0, 1)
-        );
-      }
-    } else {
-      for (const clean of inputValueArr) {
-        arr.push(
-          clean
-            .substring(clean.indexOf('xyz') + 3)
-            .split('#'[0])
-            .slice(0, 1)
-        );
-      }
+    for (const clean of inputValueArr) {
+      arr.push(
+        clean
+          .substring(clean.indexOf('.xyz') + 4)
+          .split('?'[0])
+          .slice(0, 1)
+      );
     }
   }
+} else {
+  if (inputField.value.includes(com)) {
+    for (const clean of inputValueArr) {
+      arr.push(
+        clean
+          .substring(clean.indexOf('.com') + 4)
+          .split('#'[0])
+          .slice(0, 1)
+      );
+    }
+  } else {
+    for (const clean of inputValueArr) {
+      arr.push(
+        clean
+          .substring(clean.indexOf('.xyz') + 4)
+          .split('#'[0])
+          .slice(0, 1)
+      );
+    }
+  }
+}
 
-  p.textContent = `(${arr.join('|')})`;
+p.textContent = `(${arr.join('|')})`;
+console.log(`(${arr.join('|')})`.length);
 }
 
 /* ------NEXT------
